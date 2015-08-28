56a550a2bb94bac3c61f8f7cc909488ee720b4e1
tomduhourq <t.duhourq@gmail.com>
2015-08-28 18:28:10 -0300

    Delete old solutions

--- mr-python/src/variance/VarianceMapper.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-for line in sys.stdin:
-    column = -1
-    nums = line.rstrip().split(',')
-    for n in nums:
-        column += 1
-        # map numbers to (column, num)
-        print '%s\t%s' % (column, n)
